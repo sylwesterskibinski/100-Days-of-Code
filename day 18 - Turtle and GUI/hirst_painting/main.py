04ff08a961e51e5ae6018df338d7919598756300
Sylwester Skibiński <skibinskisylwester@gmail.com>
2024-05-14 22:46:07 +0200

    Added method for creating dots and finishing the project

--- day 18 - Turtle and GUI/hirst_painting/main.py
+++ day 18 - Turtle and GUI/hirst_painting/main.py
@@ -1,5 +1,6 @@
 import colorgram
-
+from turtle import Turtle, Screen
+from random import choice
 colors = colorgram.extract('C:/Users/skibi/Desktop/100 Days of Code/day 18 - Turtle and GUI/hirst_painting/dots.jpg', 10)
 
 rgb_list = []
@@ -11,7 +12,26 @@ for color in colors:
     rgb = (r, g, b)
     rgb_list.append(rgb)
 
-print("Original RGB List:", rgb_list)
-
 filtered_rgb_list = [rgb for rgb in rgb_list if not (rgb[0] > 200 and rgb[1] > 200 and rgb[2] > 200)]
+
+timmy = Turtle()
+screen = Screen()
+screen.colormode(255)
+timmy.penup()
+timmy.setpos(-200,-100)
+
+for _ in range(10):
+    def make_dots_in_row():
+        for _ in range(10):
+            dot_color = choice(filtered_rgb_list)
+            timmy.dot(15,dot_color)
+            timmy.forward(30)
+        
+    make_dots_in_row()  
+    position = timmy.position()
+    timmy.setposition(((position[0])-300),(position[1])+30)
+
+      
+
+screen.exitonclick()
 
