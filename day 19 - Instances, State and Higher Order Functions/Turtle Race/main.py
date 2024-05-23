ae969f44edc13042134d22f8dd986d09ec99bc0f
Sylwester Skibiński <skibinskisylwester@gmail.com>
2024-05-23 13:02:50 +0200

    Final code for turtle race

--- day 19 - Instances, State and Higher Order Functions/Turtle Race/main.py
+++ day 19 - Instances, State and Higher Order Functions/Turtle Race/main.py
@@ -1,4 +1,7 @@
 from turtle import Turtle,Screen
+import random
+
+is_race_on = False
 
 screen = Screen()
 screen.setup(width=500,height=400)
@@ -14,5 +17,21 @@ for i in range(6):
     new_turtle.penup()
     new_turtle.color(colors[i])
     new_turtle.goto(x=-230, y=y_positions[i])
+    turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won, {winning_color} color won")
+            else:
+                print(f"You've lost, {winning_color} color won")
+        random_distance = random.randint(0,10)
+        turtle.forward(random_distance)
 
 screen.exitonclick()
