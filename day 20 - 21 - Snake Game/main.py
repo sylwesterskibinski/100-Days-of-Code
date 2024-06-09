ed8c957971d137cd3c22c288d8a0ff6ba4baa806
Sylwek <skibinskisylwester@gmail.com>
2024-06-09 22:39:22 +0200

    Final status of snake project

--- day 20 - 21 - Snake Game/main.py
+++ day 20 - 21 - Snake Game/main.py
@@ -31,6 +31,7 @@ while game_is_on:
     #Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.clear()
         scoreboard.score += 1
         scoreboard.write(f"Score: {scoreboard.score}",align="center",font=("Arial",24))
@@ -39,5 +40,13 @@ while game_is_on:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
+
+    #Detect collision with tail.    
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick() 
